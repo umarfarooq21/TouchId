c6804098f1bf8a38855565867ab162243041d236
Muhammad Umar <muhammad@inabia.com>
2019-03-01 11:08:51 +0500

    Initial Commit

---
+++ BioMetric/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BioMetric
+//
+//  Created by Muhammad Umair Qureshi on 3/1/19.
+//  Copyright © 2019 Umar Farooq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
